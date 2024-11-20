6b474db7bc89182704e336f6e1b0ec88e305d05a
Nikolay Halachev <nikol@DESKTOP-KMMU50S>
2024-11-20 16:55:01 +0200

    Repository added

---
+++ TechStore.Infrastructure/Common/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechStore.Infrastructure.Common
+{
+    public interface IRepository
+    {
+        IQueryable<T> AllAsReadOnly<T>(Expression<Func<T, bool>> condition)
+            where T : class;
+    }
+}
---
+++ TechStore.Infrastructure/Common/Repository.cs
@@ -0,0 +1,28 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using TechStore.Infrastructure.Data;
+
+namespace TechStore.Infrastructure.Common
+{
+    public class Repository : IRepository
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public Repository(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public IQueryable<T> AllAsReadOnly<T>(Expression<Func<T, bool>> condition)
+            where T : class
+        {
+            return this.DbSet<T>().Where(condition).AsNoTracking();
+        }
+        private DbSet<T> DbSet<T>()
+            where T : class
+        {
+            return this.dbContext.Set<T>();
+        }
+    }
+}
