e5007f478f761ca758347265e43b6d1875719ad0
Nikolay Halachev <nikol@DESKTOP-KMMU50S>
2024-11-20 16:59:05 +0200

    Extensions added

---
+++ TechStore.Core/Extensions/TechStoreServiceCollectionExtension.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using TechStore.Core.Contracts;
+using TechStore.Core.Services;
+using TechStore.Infrastructure.Common;
+
+
+namespace TechStore.Core.Extensions
+{
+    public static class TechStoreServiceCollectionExtension
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IRepository, Repository>();
+            services.AddScoped<ILaptopService, LaptopService>();
+            return services;
+        }
+    }
+}
