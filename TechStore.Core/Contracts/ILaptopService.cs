40eb19a37da23123ae6546fe4b5372a02af776b0
Nikolay Halachev <nikol@DESKTOP-KMMU50S>
2024-11-20 16:58:42 +0200

    Contracts added

---
+++ TechStore.Core/Contracts/ILaptopService.cs
@@ -0,0 +1,9 @@
+﻿using TechStore.Core.Models.Laptop;
+
+namespace TechStore.Core.Contracts
+{
+    public interface ILaptopService
+    {
+        Task<IEnumerable<LaptopExportViewModel>> GetAllLaptopsAsync();
+    }
+}
